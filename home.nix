7bc65b8d9f2267d990dd37e5e43d182dc9550781
exor2008 <exorsteam2008@gmail.com>
2024-08-28 00:04:00 +0000

    Sway windows colorscheme

--- home.nix
+++ home.nix
@@ -13,7 +13,9 @@ in
 
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  imports = [ nixvim.homeManagerModules.nixvim ];
+  imports = [
+    nixvim.homeManagerModules.nixvim
+  ];
 
   home.packages = with pkgs; [
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
@@ -135,6 +137,37 @@ in
         }
       ];
       menu = "${pkgs.wofi}/bin/wofi --show drun";
+      colors = {
+        background = "#152528";
+        focused = {
+          background = "#293E40";
+          border = "#FF8349";
+          childBorder = "#FF8349";
+          text = "#E6EAEA";
+          indicator = "#FF8349";
+        };
+        focusedInactive = {
+          background = "#293E40";
+          border = "#5A93AA";
+          childBorder = "#5A93AA";
+          text = "#E6EAEA";
+          indicator = "#5A93AA";
+        };
+        placeholder = {
+          background = "#293E40";
+          border = "#FF8349";
+          childBorder = "#FF8349";
+          text = "#E6EAEA";
+          indicator = "#FF8349";
+        };
+        unfocused = {
+          background = "#152528";
+          border = "#2D4F56";
+          childBorder = "#2D4F56";
+          text = "#E6EAEA";
+          indicator = "#2D4F56";
+        };
+      };
       window = {
         border = 3;
         # titlebar = false;
