e2824ea417e5afc908d0d5c3ec6f61fd334ac901
Ian <exor2008@yandex.ru>
2024-09-07 00:11:17 -0400

    sway: disable conf check

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -6,5 +6,6 @@
     systemd.enable = true;
     package = pkgs.swayfx;
     wrapperFeatures.gtk = true;
+    checkConfig = false;  # workaround for https://discourse.nixos.org/t/sway-fails-with-cannot-create-gles2-renderer-after-update/45703
   };
 }
