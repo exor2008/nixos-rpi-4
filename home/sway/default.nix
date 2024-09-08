18e9d0597d330c0912b758ce75d821daf52353cc
Ian <exor2008@yandex.ru>
2024-09-07 22:35:18 -0400

    fix: fetchurl

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let 
-  wallpaper = fetchurl {
+  wallpaper = builtins.fetchurl {
     url = "https://github.com/exor2008/stardog/blob/main/wallpaper.png"; # replace with the actual image URL
     sha256 = "0h7ax86idilqw87aqxamvlmchr0slnjchimlwmmjxy6hq4jh9yx4"; # Replace with actual hash
   };
