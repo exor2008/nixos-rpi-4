d5458dbd72ba125a7c034783d06c70e338c61022
exor2008 <exorsteam2008@gmail.com>
2024-09-07 23:10:20 -0400

    fix: fetchurl

--- home/sway/output.nix
+++ home/sway/output.nix
@@ -1,14 +1,14 @@
-{ config, pkgs, lib, fetchurl, ... }:
+{ pkgs, ... }:
 
 let 
-  wallpaper = fetchurl {
+  wallpaper = pkgs.fetchurl {
     url = "https://github.com/exor2008/stardog/blob/main/wallpaper.png"; # replace with the actual image URL
     sha256 = "0h7ax86idilqw87aqxamvlmchr0slnjchimlwmmjxy6hq4jh9yx4"; # Replace with actual hash
   };
 in 
 {
   wayland.windowManager.sway = {
-    config = rec {
+    config = {
       output = {
         HDMI-A-1 = {
           mode = "1920x1080@60Hz bg ${wallpaper} stretch";
