f8df930450f896525d90653cc7b3b13dfffd2f4d
Ian <exor2008@yandex.ru>
2024-09-07 22:46:44 -0400

    fix: fetchurl

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -1,11 +1,5 @@
-{ pkgs, fetchurl, ... }:
+{ pkgs, ... }:
 
-let 
-  wallpaper = fetchurl {
-    url = "https://github.com/exor2008/stardog/blob/main/wallpaper.png"; # replace with the actual image URL
-    sha256 = "0h7ax86idilqw87aqxamvlmchr0slnjchimlwmmjxy6hq4jh9yx4"; # Replace with actual hash
-  };
-in 
 {
   wayland.windowManager.sway = {
     enable = true;
