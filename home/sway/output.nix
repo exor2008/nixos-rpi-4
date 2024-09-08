30cc590897b24af8d66667e3e54a6a503cb92a73
exor2008 <exorsteam2008@gmail.com>
2024-09-08 00:20:41 -0400

    fetchurl -> fetchFromGithub

--- home/sway/output.nix
+++ home/sway/output.nix
@@ -1,17 +1,19 @@
 { pkgs, ... }:
 
 let 
-  wallpaper = pkgs.fetchurl {
-    url = "https://github.com/exor2008/stardog/blob/main/wallpaper.png"; # replace with the actual image URL
-    sha256 = "0h7ax86idilqw87aqxamvlmchr0slnjchimlwmmjxy6hq4jh9yx4"; # Replace with actual hash
-  };
+  wallpaper = pkgs.fetchFromGitHub {
+    owner = "exor2008";
+    repo = "stardog";
+    rev = "d8de1af527100b9962e64a77d6eef8f31b19931c";
+    hash = "sha256-mU8SinH+TXJYsxdBrqQY6/Wrf6gjB9oQthVcnpiNx/8=";
+  }; 
 in 
 {
   wayland.windowManager.sway = {
     config = {
       output = {
         HDMI-A-1 = {
-          mode = "1920x1080@60Hz bg ${wallpaper} stretch";
+          mode = "1920x1080@60Hz bg ${wallpaper}/wallpaper.png stretch";
         };
       };
     };
