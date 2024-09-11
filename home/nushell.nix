19ffa12a52fb9248a8a1c069b516400822fd260b
exor2008 <exorsteam2008@gmail.com>
2024-09-11 02:15:00 -0400

    nushell: disable welcome banner

--- home/nushell.nix
+++ home/nushell.nix
@@ -1,14 +1,24 @@
-{ config, pkgs, lib, ... }:
+{ ...  }:
 
 {
   programs = {
     nushell = {
       enable = true;
+
       shellAliases = {
         # bonsai = ''cbonsai -il -b 2 -c "o-o,O-o,o-O"'';
         # matrix = "~/.cargo/bin/rusty-rain -s -C 255,131,73";
         # nnn = "nnn -H";
       };
+
+      extraConfig = ''
+        $env.config = {
+          show_banner: false
+        }'';
+
+      extraLogin = ''
+        echo "Ololo"
+      '';
     };
   };
 }
