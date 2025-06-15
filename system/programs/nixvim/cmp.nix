a0aa3d93675a5fa8602cf6d8ae9e489b397ca4c9
exor2008 <exorsteam2008@gmail.com>
2025-06-15 19:12:48 -0400

    nixvim: lualine, colorizer, which-key, autoclose

--- system/programs/nixvim/cmp.nix
+++ system/programs/nixvim/cmp.nix
@@ -5,12 +5,13 @@
     plugins = {
       cmp-nvim-lsp.enable = true;
       cmp-buffer.enable = true;
-      cmp-path.enable = true; 
+      cmp-path.enable = true;
       cmp-cmdline.enable = true;
+      cmp_luasnip.enable = true;
       cmp = {
         enable = true;
         autoEnableSources = true;
-	settings = {
+        settings = {
           mapping = {
             "<C-j>" = "cmp.mapping.select_next_item()";
             "<C-k>" = "cmp.mapping.select_prev_item()";
@@ -46,13 +47,21 @@
             "<CR>" = "cmp.mapping.confirm({ select = false })"; # Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })";
           };
-	  sources = [
-	    { name = "nvim_lsp"; }
-	    { name = "path"; }
-	    { name = "buffer"; }
-	  ];
-	};
+          snippet = {
+            expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "luasnip"; }
+          ];
+        };
       };
     };
+
+    extraConfigLua = ''
+      luasnip = require("luasnip")
+    '';
   };
 }
--- system/programs/nixvim/default.nix
+++ system/programs/nixvim/default.nix
@@ -17,8 +17,24 @@
       shiftwidth = 2;
     };
 
-    plugins = { treesitter.enable = true; };
+    plugins = {
+      treesitter.enable = true;
+      autoclose.enable = true;
+      which-key.enable = true;
+      colorizer.enable = true;
+      lualine = {
+        enable = true;
+        settings.options.theme = "moonfly";
+      };
+    };
   };
 
-  imports = [ ./lsp.nix ./cmp.nix ./conform.nix ./telescope.nix ./yazi.nix ];
+  imports = [
+    ./lsp.nix
+    ./cmp.nix
+    ./conform.nix
+    ./telescope.nix
+    ./yazi.nix
+    ./luasnip.nix
+  ];
 }
